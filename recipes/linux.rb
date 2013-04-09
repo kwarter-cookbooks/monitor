1d7125f2bac5c48579849543ef02cca881122b0d
Gilles Devaux <gilles.devaux@gmail.com>
2013-04-08 17:32:13 -0700

    fix merge conflicts

--- recipes/linux.rb
+++ recipes/linux.rb
@@ -6,7 +6,6 @@ monitor_check 'disk_usage' do
   standalone true
   interval 30
   subscribers ['base']
-  notification 'Disk usage is too high.'
 end
 
 monitor_check 'memory' do
@@ -16,25 +15,22 @@ monitor_check 'memory' do
   standalone true
   interval 30
   subscribers ['base']
-  notification 'Memory usage is too high.'
 end
 
 monitor_check 'swap' do
-  file '/system/check-swap.sh -w '
+  file '/system/check-mem.rb'
   command '--swap -w 60 -c 50'
   handlers ['default']
   standalone true
   interval 30
   subscribers ['base']
-  notification 'Swap usage is too high.'
 end
 
 monitor_check 'load' do
-  file '/etc/sensu/plugins/system/check-load.rb'
+  file '/system/check-load.rb'
   command '-w 10,15,25 -c 15,20,30'
   handlers ['default']
   standalone true
   interval 30
   subscribers ['base']
-  notification 'Load is too high.'
 end
