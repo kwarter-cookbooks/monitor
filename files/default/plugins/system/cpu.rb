752a3a2c18ffe9f2685787ba9a6508b0687b028e
Gilles Devaux <gilles.devaux@gmail.com>
2013-04-09 18:05:39 -0700

    use proper superclass

--- files/default/plugins/system/cpu.rb
+++ files/default/plugins/system/cpu.rb
@@ -14,7 +14,7 @@ require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'sensu-plugin/metric/cli'
 require 'socket'
 
-class CPU < Sensu::Plugin::CLI
+class CPU < Sensu::Plugin::Check::CLI
 
   option :warning,
          :description => "Warning level in %",
