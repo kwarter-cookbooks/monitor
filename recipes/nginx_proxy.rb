aeb944d7f6f18dd723d59d64d24a96b3cabb3bc8
tonyjchong <tony.chong@kwarter.com>
2013-07-31 15:44:04 -0700

    added ssl support

--- recipes/nginx_proxy.rb
+++ recipes/nginx_proxy.rb
@@ -2,20 +2,27 @@ include_recipe 'nginx'
 
 if node['monitor']['nginx']['ssl']
   proxy = data_bag_item('sensu', 'proxy')
-
-  directory File.join(node['nginx']['dir'], 'ssl') do
-    owner 'root'
-    group 'root'
-    mode '0755'
+  ssl_record = proxy[node[:ec2][:placement_availability_zone][0...-1]]
+  unless ssl_record
+    # take a default one
+    ssl_record = proxy.reject {|key, value| key == "id"}
+    ssl_record = ssl_record[ssl_record.keys[0]]
   end
 
-  %w(key cert).each do |item|
-    file File.join(node['nginx']['dir'], 'ssl', "sensu.#{item}") do
-      content proxy['ssl'][item]
-      mode 0644
+  if ssl_record
+    directory File.join(node['nginx']['dir'], 'ssl') do
+      owner 'root'
+      group 'root'
+      mode '0755'
+    end
+
+    %w(key cert).each do |item|
+      file File.join(node['nginx']['dir'], 'ssl', "sensu.#{item}") do
+        content ssl_record[item]
+        mode 0644
+      end
     end
   end
-
 end
 
 template File.join(node['nginx']['dir'], 'sites-available', 'sensu') do
@@ -24,7 +31,7 @@ template File.join(node['nginx']['dir'], 'sites-available', 'sensu') do
   group 'root'
   mode '0644'
   variables :ssl_key  => File.join(node['nginx']['dir'], 'ssl', 'sensu.key'),
-            :ssl_cert => File.join(node['nginx']['dir'], 'ssl', 'sensu.crt')
+            :ssl_cert => File.join(node['nginx']['dir'], 'ssl', 'sensu.cert')
   notifies :reload, resources(:service => 'nginx')
 end
 
